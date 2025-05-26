a7225025a6d8cce54d8c3dd0887528c129fc40d1
Nihafis Hasamoh <nihafis3603@gmail.com>
2025-05-26 17:23:11 +0700

    start aboutme sectuion

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,7 @@
+export const AboutSection = () => {
+  return (
+    <section id="about" className="container mx-auto px-4 py-16">
+      <h2 className="text-3xl font-bold text-center mb-8">About Me</h2>
+    </section>
+  );
+};
